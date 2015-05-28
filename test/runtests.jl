a8191225799beaace0355bc97eca0c0cd1e9abef
Emmanuel Raviart <emmanuel@raviart.com>
2015-05-28 10:35:24 +0200

    Change license from Apache 2 to MIT.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,12 @@
-# Slugify -- A library that simplifies a text to an ASCII subset
+# Slugify.jl -- A library that simplifies a text to an ASCII subset
 # By: Emmanuel Raviart <emmanuel@raviart.com>
 #
 # Copyright (C) 2015 Emmanuel Raviart
 # https://github.com/eraviart/Slugify.jl
 #
-# This file is part of Slugify.
+# This file is part of Slugify.jl.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# The Slugify.jl package is licensed under the MIT "Expat" License.
 
 
 using Base.Test
